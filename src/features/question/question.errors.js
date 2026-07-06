f53837700c0c3c5bce4e7a9ce8844aeccea7ef12
Eduardo <cheligaedu@gmail.com>
2026-07-05 21:33:57 -0300

    question error

---
+++ src/features/question/question.errors.js
@@ -0,0 +1,5 @@
+export const QuestionErrors = {
+  NOT_FOUND: 'Questão não encontrada.',
+  CATEGORY_REQUIRED: 'A categoria da questão é obrigatória.',
+  CANNOT_DELETE_WITH_INTERACTIONS: 'Não é possível deletar uma questão que já possui respostas dos usuários.'
+}
